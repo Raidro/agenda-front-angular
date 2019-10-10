609e9f4bc3bf6836d68767c43bd7de1b4f1173f9
MATEUS PERREIRA SATIRO REYES <a9261456@rn.senac.br>
2019-10-10 20:32:17 -0300

    iniciando o deploy no heroku

---
+++ src/app/app.component.ts
@@ -0,0 +1,56 @@
+import { ContatoService } from './contato.service';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+
+class Contato {
+  id: number;
+  nome: string;
+  fone: string;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent  implements OnInit{
+
+
+
+  contato = new Contato();
+  contatos = [];
+
+  constructor(private service: ContatoService){}
+
+  ngOnInit(): void {
+    this.buscaContatos();
+  }
+
+  buscaContatos(){
+    this.service.buscaTodos().then(dados => {
+      this.contatos = dados;
+    });
+  }
+
+  salvar(formContato: NgForm) {
+
+    this.contato.nome = formContato.value.nome;
+    this.contato.fone = formContato.value.fone;
+
+    this.service.salvaContato(this.contato).then(c => {
+      // alert(`Contato ${c.nome} salvo`);
+      this.buscaContatos();
+    });
+
+    this.contato = new Contato();
+
+  }
+
+  editar(id: number) {
+    for (const c of this.contatos) {
+      console.log(c);
+    }
+  }
+
+}
